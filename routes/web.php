a188d19dee3864b6ad48171a658094dc58709f08
David Casanova <casanova.83130@gmail.com>
2024-07-04 14:22:52 +0200

    routes optimised

--- routes/web.php
+++ routes/web.php
@@ -9,25 +9,17 @@ require __DIR__ . '/auth.php';
 Route::view('/', 'home')
     ->name('home');
 
-Route::view('create', 'pages.create')
-    ->middleware(['auth'])
-    ->name('create');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-Route::post('formulaire', [PageController::class, 'store'])
-    ->middleware(['auth'])
-    ->name('formulaire.store');
+// Authenticated routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('create', 'pages.create')->name('create');
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('profile', 'profile')->name('profile');
+    Route::post('formulaire', [PageController::class, 'store'])->name('formulaire.store');
+    Route::get('/{slug}/edit', [PageController::class, 'edit'])->name('pages.edit');
+});
 
 Route::get('/pages', [PageController::class, 'index'])->name('pages');
 Route::get('/{slug}', [PageController::class, 'show'])->name('page.show');
-Route::get('/{slug}/edit', [PageController::class, 'edit'])->middleware(['auth', 'verified'])->name('pages.edit');
 Route::put('/{slug}', [PageController::class, 'update'])->name('pages.update');
 
 require __DIR__ . '/auth.php';
